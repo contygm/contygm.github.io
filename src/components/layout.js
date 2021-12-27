8dca246583153612f84d1f9e90e2e20240774464
contygm <contygm@gmail.com>
2021-12-27 15:43:08 -0800

    basic v+h centered landing

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,17 @@ const Layout = ({ location, title, children }) => {
     const rootPath = `${__PATH_PREFIX__}/`;
     const isRootPath = location.pathname === rootPath;
     let header;
+    let containerClass;
 
     if (isRootPath) {
+        containerClass = "d-flex flex-column min-vh-100 justify-content-center align-items-center";
         header = (
             <h1 className="main-heading">
                 <Link to="/">{title}</Link>
             </h1>
         );
     } else {
+        containerClass = '"container';
         header = (
             <Link className="header-link-home" to="/">
                 {title}
@@ -21,14 +24,18 @@ const Layout = ({ location, title, children }) => {
     }
 
     return (
-        <div className="container" data-is-root-path={isRootPath}>
-            <header className="global-header">{header}</header>
-            <main>{children}</main>
+        <div data-is-root-path={isRootPath}>
+            {/* <header className="global-header">{header}</header>
+            
             <footer>
                 © {new Date().getFullYear()}, Built with
                 {` `}
                 <a href="https://www.gatsbyjs.com">Gatsby</a>
-            </footer>
+            </footer> */}
+            
+            <main className={containerClass}>
+                {children}
+            </main>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,17 @@ import Bio from "../components/bio";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const BlogIndex = ({ data, location }) => {
+const Index = ({ data, location }) => {
     const siteTitle = data.site.siteMetadata?.title || `Title`;
 
     return (
         <Layout location={location} title={siteTitle}>
-            <Seo title="All posts" />
+            <Seo title="Home" />
+            <h1>Genevieve Conty</h1>
+            <p>Full-Stack Software Engineer</p>
+            <button className="btn btn-success">Resume</button>
         </Layout>
     );
 };
 
-export default BlogIndex;
+export default Index;

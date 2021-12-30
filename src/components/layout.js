691d4fde32371cd8b2576c53125e6f31bd04d1c8
contygm <contygm@gmail.com>
2021-12-30 12:37:20 -0800

    fixed blurry icons, updated fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,39 +3,45 @@ import useLocalStorage from "use-local-storage";
 import { getImage, GatsbyImage } from "gatsby-plugin-image";
 
 const Layout = ({ location, title, children, icons}) => {
-    // const blogPath = `${__PATH_PREFIX__}/blog`;
-    // const isBlog = location.pathname === blogPath;
-    
-
     const rootPath = `${__PATH_PREFIX__}/`;
-    const isRootPath = location.pathname === rootPath || location.pathname === `${__PATH_PREFIX__}/404`;
+    const blogPath = `${__PATH_PREFIX__}/blog/`;
+    const isNotBlog = location.pathname !== blogPath;
+    const isRootPath = location.pathname === rootPath;
     
+    // toggle theme
     const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const [theme, setTheme] = useLocalStorage('theme', (defaultDark ? 'dark' : 'light'));
-    
-    
     const themeToggler = () => {
         theme === 'light' ? 
             setTheme('dark') : 
             setTheme('light');
-        console.log("now it's", theme)
-        
     }
-
-    const themeIcon = theme === "light" ? `black-moon` : `white-sun`;
-
-    console.log(icons, themeIcon)
-    const imgData = icons.find((entry) => entry.name === themeIcon);
-    console.log(imgData)
-    const img = getImage(imgData.childImageSharp);
 
     let containerClass;
 
-    if (isRootPath) {
+    if (isNotBlog) {
         containerClass = "d-flex flex-column min-vh-100 justify-content-center align-items-center";
     } else {
         containerClass = "container";
     }
+
+    if (isRootPath) {
+        // get correct theme icon
+        const themeIcon = theme === "light" ? `black-moon` : `white-sun`;
+        const imgData = icons.find((entry) => entry.name === themeIcon);
+        const img = getImage(imgData.childImageSharp);
+        
+        return (
+            <div data-is-root-path={isRootPath} data-theme={theme} className="global-wrapper">
+                <button className="btn float-end" onClick={themeToggler}>
+                    <GatsbyImage image={img} alt={themeIcon}/>
+                </button>
+                <main className={containerClass}>
+                    {children}
+                </main>
+            </div>
+        );
+    } 
 
     return (
         <div data-is-root-path={isRootPath} data-theme={theme} className="global-wrapper">
@@ -45,9 +51,7 @@ const Layout = ({ location, title, children, icons}) => {
                 {` `}
                 <a href="https://www.gatsbyjs.com">Gatsby</a>
             </footer> */}
-            <button className="btn float-end" onClick={themeToggler}>
-                <GatsbyImage image={img} alt={themeIcon}/>
-            </button>
+            
             <main className={containerClass}>
                 {children}
             </main>

2237f4e4af78815c5e465d9f7abec9c0d712d607
contygm <contygm@gmail.com>
2022-01-06 09:14:05 -0800

    remove the dark/light toggle, causing build issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,42 +1,9 @@
 import * as React from "react";
-import useLocalStorage from "use-local-storage";
-import { getImage, GatsbyImage } from "gatsby-plugin-image";
 
 const Layout = ({ location, title, children, icons}) => {
     const rootPath = `${__PATH_PREFIX__}/`;
     const isRootPath = location.pathname === rootPath;
-
-    const isBrowser = typeof window !== `undefined`;
-
     const containerClass = "d-flex flex-column min-vh-100 justify-content-center align-items-center";
-
-    if (isRootPath && isBrowser) {
-        const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        const [theme, setTheme] = useLocalStorage('theme', (defaultDark ? 'dark' : 'light'));
-        const themeToggler = () => {
-            theme === 'light' ? 
-                setTheme('dark') : 
-                setTheme('light');
-        }
-        // get correct theme icon
-        const themeIcon = theme === "light" ? `black-moon` : `white-sun`;
-        const imgData = icons.find((entry) => entry.name === themeIcon);
-        const img = getImage(imgData.childImageSharp);
-        
-        return (
-            <div data-is-root-path={isRootPath} data-theme={theme} className="global-wrapper ">
-                <div className="d-flex flex-row-reverse">
-                    <button className="btn " onClick={themeToggler}>
-                        <GatsbyImage image={img} alt={themeIcon}/>
-                    </button>
-                </div>
-                
-                <main className={containerClass}>
-                    {children}
-                </main>
-            </div>
-        );
-    } 
 
     return (
         <div data-is-root-path={isRootPath} data-theme={'dark'} className="global-wrapper">
